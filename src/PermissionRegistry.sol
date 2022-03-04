a462c927ce33e3c70e2d5080ba10adbea146d275
CapronYoan <capronyoan@gmail.com>
2022-03-05 00:01:54 +0100

    test result

--- src/PermissionRegistry.sol
+++ src/PermissionRegistry.sol
@@ -1,5 +1,86 @@
 pragma solidity 0.6.2;
 
+
+
 contract PermissionRegistry {
-    //TODO: complete this contract
+
+
+    struct Item {
+        string id;
+        string description;
+    }
+
+
+    mapping(uint256  /* item index */ => mapping(uint256 /* permission */ => mapping(address /* user address */ => bool /* is granted ?*/))) private permissions; // 1= view permission, 2= edit permission, 3= grant permission
+  
+    Item[] public items;
+
+    constructor() public {
+        Item memory item  =  Item({
+            id: "",
+            description: ""
+        });
+        items.push(item);
+    }
+       
+    function createItem(string memory _id, string memory _description) public returns(bool){
+        uint256 index =  itemsLength();
+        Item memory item  =  Item({
+            id: _id,
+            description: _description
+        });
+        items.push(item);
+        permissions[index][1][msg.sender] = true;
+        permissions[index][2][msg.sender] = true;
+        permissions[index][3][msg.sender] = true;
+        return true;
+    }
+
+    function grantPermissions(string memory _id, address _user, uint256 _permission) public {
+        require(_permission >= 1 && _permission <= 3, "grantPermissions: permission integer not allowed.");
+        require(_user != address(0), "grantPermissions: user zero address not allowed.");
+        uint256 index = getIndex(_id);
+        require(index > 0, "grantPermissions: id doesn't exist.");
+        require(checkUserPermission(index, 3, msg.sender), "grantPermissions: user isn't granted.");
+        permissions[index][_permission][_user] = true;
+    }
+
+    function checkUserPermission(uint256 index, uint256 permission, address user) private view returns(bool){
+        return permissions[index][permission][user];
+    }
+
+    function getIndex(string memory _id) private view returns(uint256){
+        for(uint256 i = 0; i < itemsLength(); i++) {
+            Item memory item = items[i];
+            if(keccak256(abi.encodePacked(_id)) == keccak256(abi.encodePacked(item.id))) return i;
+        }
+        return 0;
+    }
+
+    function editItem(string memory _id, string memory _description) public returns(bool){
+        uint256 index = getIndex(_id);
+        require(index > 0, "editItem: id doesn't exist.");
+        require(checkUserPermission(index, 2, msg.sender), "editItem: user isn't allowed to edit.");
+        Item storage item = items[index];
+        item.description = _description;
+        return true;
+    }
+
+    function hasPermission(string memory _id, address _user, uint256 _permission) public view returns(bool){
+        uint256 index = getIndex(_id);
+        require(index > 0, "hasPermission: id doesn't exist.");
+        return checkUserPermission(index, _permission, _user);
+    }
+
+    function getPermissions(string memory _id, address _user) public view returns(bool[] memory) {
+        bool[] memory boolPermList = new bool[](3);
+        for(uint256 i = 0; i < 3; i++){
+            boolPermList[i] = hasPermission(_id, _user, i + 1);
+        }
+        return boolPermList;
+    }
+
+    function itemsLength() public view returns(uint256) {
+        return items.length;
+    }
 }
--- test/PermissionRegistry.test.ts
+++ test/PermissionRegistry.test.ts
@@ -1,21 +1,107 @@
 import { expect, use } from 'chai'
 import { deployContract, MockProvider, solidity } from 'ethereum-waffle'
-import { Contract } from 'ethers'
+import { Contract, utils, Wallet, ethers} from 'ethers'
 
 import PermissionRegistry from '../build/PermissionRegistry.json'
 
 use(solidity)
 
+let registry: Contract
+
+const createItem = async(_id: string, _description: string): Promise<boolean>  => {
+    return new Promise (async (resolve): Promise<void> => {
+        try {
+            
+            const tx = await registry.createItem(_id, _description)
+            const receipt = await tx.wait()
+            if(receipt.status === 1){
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (e) {
+            // console.log(e)
+            resolve(false)
+        }
+    })
+}
+
+
+
+const grantPermissions = async(_id: string, _user: Wallet, _permission: number): Promise<boolean>  => {
+    return new Promise (async (resolve): Promise<void> => {
+        try {
+            
+            const tx = await registry.grantPermissions(_id, _user.address ,_permission)
+            const receipt = await tx.wait()
+            if(receipt.status === 1){
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (e) {
+            console.log(e)
+            resolve(false)
+        }
+    })
+}
+
+
+
 describe('PermissionRegistry', (): void => { 
     //10 test wallets
     const wallets = new MockProvider().getWallets()
-    let registry: Contract
+    
 
-    beforeEach(async (): Promise<void> => {
+    before(async (): Promise<void> => {
         registry = await deployContract(wallets[0], PermissionRegistry)
     })
 
-    //TODO add tests below:
-    it('some test', async (): Promise<void> => {
+    after(() => process.exit())
+
+
+
+    let bool0: boolean
+    before(async (): Promise<void> => {
+        bool0 = await createItem("1", "blabla")
     })
+    it('should let you create item', async (): Promise<void> => {
+        expect(bool0).to.equal(true);
+    })  
+
+
+
+
+    let bool1: boolean
+    before(async (): Promise<void> => {
+        bool1 = await grantPermissions("1", wallets[1], 3)
+    })
+    it('should let you grant permission', async (): Promise<void> => {
+        expect(bool1).to.equal(true);
+    })
+
+
+
+    it('should not let you grant permission', async (): Promise<void> => {
+        const contractAddress2 = registry.connect(wallets[2]) // change to address 2
+        await expect(contractAddress2.grantPermissions("1", wallets[3].address, 3,  {gasLimit: ethers.BigNumber.from("500000")}))
+        .to.be.revertedWith("grantPermissions: user isn't granted.")
+    })
+
+
+  
+    it('should not let you edit description', async (): Promise<void> => {
+        const contractAddress2 = registry.connect(wallets[2]) // change to address 2
+        await expect(contractAddress2.editItem("1", "foo", {gasLimit: ethers.BigNumber.from("500000")}))
+        .to.be.revertedWith("editItem: user isn't allowed to edit.")
+    })
+
+
+   
+
+    
+
+
+    
+  
 })
